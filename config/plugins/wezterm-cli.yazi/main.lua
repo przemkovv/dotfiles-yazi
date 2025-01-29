8ffdab8c3e415af29c6f40e047549c73d600fd01
Przemysław Walkowiak <pwalkowiak@surgalign.com>
2025-01-29 21:06:50 +0100

    Add `g` mappings

--- config/plugins/wezterm-cli.yazi/main.lua
+++ config/plugins/wezterm-cli.yazi/main.lua
@@ -5,11 +5,14 @@ local get_current_directory = ya.sync(
 return {
   entry = function(_, job)
     local args = table.concat(job.args, " ")
+    ya.dbg("wezterm cli: " .. #job.args)
+    ya.dbg("wezterm cli: " .. args)
     local current = get_current_directory()
     if current == nil then
       return
     end
     local cmd = "wezterm.exe cli " .. args .. " --cwd " .. ya.quote(current)
+    ya.dbg("wezterm cmd: " .. cmd)
     ya.manager_emit("shell", {
       cmd,
       block = false,
