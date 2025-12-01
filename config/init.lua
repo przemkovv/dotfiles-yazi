b9e7ab23e8ebb47b722af1dd60974444adf58bb7
Przemysław Walkowiak <przemkovv@gmail.com>
2025-12-02 00:09:16 +0100

    feat: add support for external session yanking

--- config/init.lua
+++ config/init.lua
@@ -3,6 +3,10 @@ require("full-border"):setup(
     type = ui.Border.ROUNDED,
   })
 
+require("session"):setup {
+  sync_yanked = true,
+}
+
 function Linemode:size_and_mtime()
   local time = math.floor(self._file.cha.mtime or 0)
   if time == 0 then
